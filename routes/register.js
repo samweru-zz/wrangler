cd00cee6a8ba5261d1d02bb7eb9a0f814555f388
sami <sami@sami-HP-530-Notebook-PC.(none)>
2015-07-05 18:16:16 +0300

    remove mongo increment script, refactor register to check for last record insert id b4 adding new

--- routes/register.js
+++ routes/register.js
@@ -13,34 +13,35 @@ router.post('/new', function(req, res){
 	var db = req.db;
 	var body = req.body;
 
-	db.people.count({}, function(err, count){
-		
-		hash = crypto
+	db.people.find({}).limit(1).sort({_id:-1})
+		.toArray(function (err, docs) {
+
+			hash = crypto
 				.createHmac('sha1', "secret-key")
 					.update(body.password).digest('hex');
 
-		body.id = count + 1;
-		body.guid = uuid();
-		body.password = hash;
-		body.picture = "http://placehold.it/32x32";
-		body.tags = [];
-		body.friends = [];
-		body.registered = new Date().toISOString();
-		body.name = body.surname + ", " + body.othernames;
-		body.age = new Date().getFullYear() - new Date(body.dob).getFullYear();
+			body.id = docs[0].id + 1;
+			body.guid = uuid();
+			body.password = hash;
+			body.picture = "http://placehold.it/32x32";
+			body.tags = [];
+			body.friends = [];
+			body.registered = new Date().toISOString();
+			body.name = body.surname + ", " + body.othernames;
+			body.age = new Date().getFullYear() - new Date(body.dob).getFullYear();
 
-		delete body.dob
-		delete body.surname
-		delete body.othernames
+			delete body.dob
+			delete body.surname
+			delete body.othernames
 
-		db.people.insert(body, function(err, bug){
+			db.people.insert(body, function(err, bug){
 
-			if (err) 
-				res.json(500, err);
-			else 
-				res.json(201, bug);
-		});
-	});
+				if (err) 
+					res.json(500, err);
+				else 
+					res.json(201, bug);
+			});
+    	});
 });
 
 module.exports = router
