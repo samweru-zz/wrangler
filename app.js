6aed4e76c7b41bb19a74d1b4ac82376135968c2f
sami <sami@sami-HP-530-Notebook-PC.(none)>
2015-07-05 15:00:33 +0300

    working mongo incrementer function script, rename reset password script to morepass [mo]ngo [re]set [pass]word

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var session = require('express-session');
+var logger = require('express-bunyan-logger');
 var app = express();
 var path = require('path');
 var bodyParser = require("body-parser");
@@ -12,10 +13,7 @@ app.set('views', __dirname+'/views');
 app.set('view engine', 'jade');
 app.engine('jade', require('jade').__express);
 
-var index = require('./routes/index');
-var people = require('./routes/people');
-var register = require('./routes/register');
-
+// static content
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'static')));
@@ -27,13 +25,13 @@ app.use(function(req, res, next){
     next();
 });
 
-app.use(require('express-bunyan-logger')({
+// logger
+app.use(logger({
 
     name: 'logger',
     streams: [{
 
         level: 'info',
-        // stream: stream
         stream: process.stdout
     }]
 }));
@@ -42,9 +40,11 @@ app.use(require('express-bunyan-logger')({
 app.use(session({ secret: 'secret-sess-key', 
 					cookie: { maxAge: 60000 }}));
 
-app.use('/', index);
-app.use('/people', people);
-app.use('/register', register);
+//import routes
+app.use('/', require('./routes/index'));
+app.use('/people', require('./routes/people'));
+app.use('/register', require('./routes/register'));
 
+//start server
 app.listen(3333);
 console.log('Running on port 3333!');
