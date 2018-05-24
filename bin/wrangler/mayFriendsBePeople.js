700b7e8bbb97e78a4e7fd5ea1983751c1976fe47
samweru <samweru@gmail.com>
2018-05-24 16:52:38 +0300

    intersection btwn friends and peoples

---
+++ bin/wrangler/mayFriendsBePeople.js
@@ -0,0 +1,24 @@
+conn = new Mongo()
+db = conn.getDB("wrangler")
+
+var distinctFriends = []
+var allPeople = []
+
+db.people.find().forEach(function(person){
+
+	allPeople.push(person.name)
+
+	person.friends.forEach(function(friend){
+
+		//Find distinct friends records
+		if(distinctFriends.indexOf(friend.name) < 0)
+			distinctFriends.push(friend.name)
+	})
+})
+
+print("All peoples records: "+ allPeople.length)
+print("All distinct friends: "+ distinctFriends.length)
+
+var peopleFriends = allPeople.filter(function(x) { return distinctFriends.indexOf(x) > -1 })
+
+print("All friend with people records: "+ peopleFriends.length)
